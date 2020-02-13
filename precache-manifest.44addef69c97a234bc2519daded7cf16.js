d363b7b12d524d7c969660b6acbf2fc010aab81f
Juan Carlos Felizzola <jfelizzola@gasolutions.com.co>
2020-02-13 18:14:30 -0500

    Updates

---
+++ precache-manifest.44addef69c97a234bc2519daded7cf16.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "766989bb42ed8b815533cb74433dc102",
+    "url": "/fast-delivery/index.html"
+  },
+  {
+    "revision": "10ee25fead379739e790",
+    "url": "/fast-delivery/static/css/2.1af7a3c5.chunk.css"
+  },
+  {
+    "revision": "4390b21a0436c82adf3f",
+    "url": "/fast-delivery/static/css/main.149e87f6.chunk.css"
+  },
+  {
+    "revision": "10ee25fead379739e790",
+    "url": "/fast-delivery/static/js/2.7292691a.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/fast-delivery/static/js/2.7292691a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4390b21a0436c82adf3f",
+    "url": "/fast-delivery/static/js/main.ba910f18.chunk.js"
+  },
+  {
+    "revision": "b49e79585d924aa5acc1",
+    "url": "/fast-delivery/static/js/runtime-main.dd73ceb7.js"
+  },
+  {
+    "revision": "85e5ce30c3d5c1608842824c274c61d8",
+    "url": "/fast-delivery/static/media/image.85e5ce30.jpg"
+  },
+  {
+    "revision": "e5950b14402375a52414b913d10d8203",
+    "url": "/fast-delivery/static/media/logo.e5950b14.png"
+  }
+]);
